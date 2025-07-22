bbf00bae050e2dafe52a6ded298de58778d29c23
compf <timo.schoemaker97@outlook.de>
2025-07-22 18:11:10 +0200

    filtering pokemon via german name works

--- ResearchTaskLib/PokedexEntry.cs
+++ ResearchTaskLib/PokedexEntry.cs
@@ -4,6 +4,7 @@ public class PokedexEntry
 {
     public int Number{ get; set; }
     public string Name { get; set; }
+    public string GermanName{ get; set; }
     
     public bool? EvolvesFrom{ get; set; }
     public IList<ResearchTaskEntry> ResearchTasks { get; set; }
